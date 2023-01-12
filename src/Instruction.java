f9a57bdec08ca5825766d3fb5a4fa8dc296641c9
16466 <16466@LAPTOP-2U2N3SO0.mynetworksettings.com>
2023-01-12 16:02:39 -0500

    Second commit

---
+++ src/Instruction.java
@@ -0,0 +1,21 @@
+
+public class Instruction {
+
+}
+
+
+
+
+
+// to commit changesto local repository 
+//1- right click on the project 
+// 2-go to team 
+//2-go to commit
+// Git staging is opened
+
+// we have unstaged, staged commit message
+// unstaged changes will contain all files which are not part of your local repository
+// commit a file to local repository we have to add that file to staged changes
+// or 
+// I made some changes.
+//
